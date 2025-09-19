c1b9a81c5a17f2903b24ea36fe949758e699ff95
Chaitanya Konge <chaitanyakonge2002@gmail.com>
2025-09-19 12:34:40 +0530

    Add failing test for empty string returning 0

--- test/StringCalculatorTest.java
+++ test/StringCalculatorTest.java
@@ -1,6 +1,10 @@
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 public class StringCalculatorTest {
-
+	@Test
+    	void testEmptyStringReturnsZero() {
+        	StringCalculator calc = new StringCalculator();
+        	assertEquals(0, calc.add(""), "Empty string should return 0");
+    	}
 }
 
